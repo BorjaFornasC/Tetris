8b3e7f7fced1e1ddeea8c09b489625a785c86e9e
alu10784049 <alu10784049@ti3-04>
2023-03-02 20:53:49 +0100

    Initial Commit

---
+++ src/main/java/org/ieselcaminas/tetris/Score.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.ieselcaminas.tetris;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ *
+ * @author victor
+ */
+public class Score implements Comparable<Score>, Serializable{
+    
+    private String name;
+    private int score;
+    private Calendar date;
+    private int level;
+
+    public Score(String name, int score, Calendar date, int level) {
+        this.name = name;
+        this.score = score;
+        this.date = date;
+        this.level = level;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+ 
+    
+    @Override
+    public int compareTo(Score t) {
+       Integer i = score;
+       Integer i2 = t.score;
+       return i.compareTo(i2);
+    }
+    
+}
